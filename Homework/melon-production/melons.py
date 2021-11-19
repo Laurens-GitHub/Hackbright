b35a54bc07088deb87ed3c8e058dcc46ee3f9176
Lauren Edwards <Lauren@LEDwards.co>
2021-11-19 18:36:26 +0000

    Finished melon production homework

---
+++ Homework/melon-production/melons.py
@@ -0,0 +1,61 @@
+import robots
+
+
+class Melon:
+    """Melon."""
+
+    def __init__(self, melon_type):
+        """Initialize melon.
+
+        melon_type: type of melon being built.
+        """
+
+        self.melon_type = melon_type
+        self.weight = 0.0
+        self.color = None
+        self.stickers = []
+
+    def prep(self):
+        """Prepare the melon."""
+
+        robots.cleanerbot.clean(self)
+        robots.stickerbot.apply_logo(self)
+
+    def __str__(self):
+        """Print out information about melon."""
+
+        if self.weight <= 0:
+            return self.melon_type
+        else:
+            return f"{self.color} {self.weight:.2f} lbs {self.melon_type}"
+
+
+class Squash(Melon):
+    """Squash"""
+
+    def __init__(self, melon_type = "squash"):
+        """Initialize squash.
+
+        melon_type: type of squash being built.
+        """
+
+        self.melon_type = melon_type
+        self.weight = 0.0
+        self.color = None
+        self.stickers = []
+
+    def prep(self):
+        """Prepare the squash."""
+
+        robots.cleanerbot.clean(self)
+        robots.stickerbot.apply_logo(self)
+        robots.painterbot.paint(self)
+
+    def __str__(self):
+        """Print out information about squash."""
+
+        if self.weight <= 0:
+            return self.melon_type
+        else:
+            return f"{self.color} {self.weight:.2f} lbs {self.melon_type}"
+
