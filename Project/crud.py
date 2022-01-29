adad69a3fc12b4871f08ae6b47be92c0b8368344
Lauren Edwards <Lauren@LEDwards.co>
2022-01-29 17:15:16 +0000

    wrote functions for crud.py

--- Project/crud.py
+++ Project/crud.py
@@ -1,12 +1,17 @@
 """CRUD operations."""
 
-from model import db, User, Movie, Rating, connect_to_db
+from model import db, User, Stock, User_stock, connect_to_db
 
 
-def create_user(email, password):
+def create_user(first_name, last_name, email, password):
     """Create and return a new user."""
 
-    user = User(email=email, password=password)
+    user = User(
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        password=password
+    )
 
     return user
 
@@ -29,38 +34,37 @@ def get_user_by_email(email):
     return User.query.filter(User.email == email).first()
 
 
-def create_movie(title, overview, release_date, poster_path):
-    """Create and return a new movie."""
+def create_stock(symbol, company):
+    """Create and return a new stock."""
 
-    movie = Movie(
-        title=title,
-        overview=overview,
-        release_date=release_date,
-        poster_path=poster_path,
+    stock = Stock(
+        symbol=symbol,
+        company=company
     )
 
-    return movie
+    return stock
+
+def create_user_stock(user, stock, date_saved):
+    """Create and return a new user stock."""
+
+    user_stock = User_stock(
+            user=user,
+            stock=stock,
+            date_saved=date_saved
+    )
+
+    return user_stock
+
+def get_stocks():
+    """Return all stocks."""
+
+    return Stock.query.all()
 
 
-def get_movies():
-    """Return all movies."""
+# def get_movie_by_id(movie_id):
+#     """Return a movie by primary key."""
 
-    return Movie.query.all()
-
-
-def get_movie_by_id(movie_id):
-    """Return a movie by primary key."""
-
-    return Movie.query.get(movie_id)
-
-
-def create_rating(user, movie, score):
-    """Create and return a new rating."""
-
-    rating = Rating(user=user, movie=movie, score=score)
-
-    return rating
-
+#     return Movie.query.get(movie_id)
 
 if __name__ == "__main__":
     from server import app
